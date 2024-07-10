de3cea1591857ddaa7b1ebf1a85811b75761a59d
= <=>
2024-07-10 16:15:26 +0100

    Some adjustments

--- src/components/CategoryView.jsx
+++ src/components/CategoryView.jsx
@@ -11,15 +11,21 @@ export default function CategoryView({ isHidden }) {
         <p className=" text-[#6E6E6E]">Shop Premium and Branded Products</p>
       </section>
       <div className={isHidden + " flex flex-col p-8 gap-4"}>
-        <h1 className="font-bold text-xl text-[#6E6E6E]">Body Lotions</h1>
+        <h1 className="font-bold text-xl text-[#6E6E6E] text-center md:text-left">
+          Body Lotions
+        </h1>
         <ProductList />
       </div>
       <div className={isHidden + " flex flex-col p-8 gap-4"}>
-        <h1 className="font-bold text-xl text-[#6E6E6E]">Cosmetics</h1>
+        <h1 className="font-bold text-xl text-[#6E6E6E] text-center md:text-left">
+          Cosmetics
+        </h1>
         <ProductList />
       </div>
       <div className={isHidden + " flex flex-col p-8 gap-4"}>
-        <h1 className="font-bold text-xl text-[#6E6E6E]">Perfumes</h1>
+        <h1 className="font-bold text-xl text-[#6E6E6E] text-center md:text-left">
+          Perfumes
+        </h1>
         <ProductList />
       </div>
     </div>
