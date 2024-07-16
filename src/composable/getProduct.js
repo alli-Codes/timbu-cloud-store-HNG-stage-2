76bbea8c39e8085e970b566f1387a7c6bc736834
= <=>
2024-07-16 10:31:48 +0100

    Made chnages to vite config

--- src/composable/getProduct.js
+++ src/composable/getProduct.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchData = async (id) => {
-  const API_URL = `api/products/${id}?organization_id=7969044c6ce14183923973947f6b847f&Appid=DM5EKOPTYGT0T8C&Apikey=80ef3d8f25974c2e9d862d383a4a993e20240712202351870290`; // Replace with your API URL
+  const API_URL = `https://api.timbu.cloud/products/${id}?organization_id=7969044c6ce14183923973947f6b847f&Appid=DM5EKOPTYGT0T8C&Apikey=80ef3d8f25974c2e9d862d383a4a993e20240712202351870290`; // Replace with your API URL
   try {
     const response = await axios.get(API_URL, {
       headers: {
