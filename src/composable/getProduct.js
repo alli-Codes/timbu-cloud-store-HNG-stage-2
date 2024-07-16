26eea25780c1d7d1eb95e6e8a86a10775edd3648
= <=>
2024-07-16 11:05:23 +0100

    Making some changes

--- src/composable/getProduct.js
+++ src/composable/getProduct.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchData = async (id) => {
-  const apiBaseUrl = import.meta.env.VITE_BASE_URL;
+  const apiBaseUrl = import.meta.env.VITE_APP_BASE_URL;
   const apiKey = import.meta.env.VITE_APP_API_KEY;
   const appId = import.meta.env.VITE_APP_APP_ID;
   const orgId = import.meta.env.VITE_APP_ORGANIZATION_ID;
